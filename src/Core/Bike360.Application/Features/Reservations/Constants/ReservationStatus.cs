1040c39937dd641a053ab5366c33055676611d52
Krzysztof Szczypka <72348010+szczypkakrzys@users.noreply.github.com>
2024-09-26 20:50:05 +0200

    Expand reservations statuses options

--- src/Core/Bike360.Application/Features/Reservations/Constants/ReservationStatus.cs
+++ src/Core/Bike360.Application/Features/Reservations/Constants/ReservationStatus.cs
@@ -3,4 +3,10 @@
 public static class ReservationStatus
 {
     public const string Pending = "Pending";
+    public const string InProgress = "In progress";
+    public const string Returned = "Returned";
+    public const string Completed = "Completed";
+    public const string Cancelled = "Cancelled";
+    public const string Expired = "Expired";
+    public const string Overdue = "Overdue";
 }
