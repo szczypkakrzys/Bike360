b68fe20801b2df5420eb56cf515335c5742bb5d4
Krzysztof Szczypka <72348010+szczypkakrzys@users.noreply.github.com>
2024-10-11 22:06:03 +0200

    custom 404 page

--- src/UI/Bike360.UI/src/App.jsx
+++ src/UI/Bike360.UI/src/App.jsx
@@ -7,12 +7,14 @@ import {
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import BikesPage from './pages/BikesPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={ <MainLayout /> }>
       <Route index element={ <HomePage /> }/>
       <Route path='/bikes' element={ <BikesPage /> }/>
+      <Route path='*' element={ <NotFoundPage /> }/>
     </Route>
   )
 );
