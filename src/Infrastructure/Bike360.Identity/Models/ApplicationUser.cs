ec179a4fa15f770113f7af2fd3522099909669d0
Krzysztof Szczypka <72348010+szczypkakrzys@users.noreply.github.com>
2025-01-08 22:16:32 +0100

    Remove unused identity project from repo

--- src/Infrastructure/Bike360.Identity/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Bike360.Identity.Models;
-
-public class ApplicationUser : IdentityUser
-{
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-}
